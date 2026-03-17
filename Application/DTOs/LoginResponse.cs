5c02f747acbc2e6444ec7116b90b17ae0a7004be
Bruno Henrique <bruno.ferreira@ascsolutions.com.br>
2026-03-16 22:31:54 -0300

    Remove obsolete LoginResponse DTO from the application responses

--- Application/DTOs/LoginResponse.cs
+++ Application/DTOs/LoginResponse.cs
@@ -1,1 +1,10 @@
-
+namespace PJ_API.Application.Responses
+{
+    public class LoginResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
